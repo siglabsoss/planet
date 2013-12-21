0c996a77357669ec6ea8834c8d4f95e5752fe33f
Benjamin Morse <bjmorse@gmail.com>
2013-12-20 16:06:43 -0800

    * can correctly build nested json object from flat database

--- client/views/lump.js
+++ client/views/lump.js
@@ -14,3 +14,89 @@ Template.groups.rendered = function() {
     });
 
 }
+
+// returns true for success, false for failure
+function attachChild(output, child)
+{
+    for(var i in output)
+    {
+        if( output[i]._id == child.parent )
+        {
+            output[i].children.push(child);
+            return true;
+        }
+        if( output[i].children.length != 0 )
+        {
+            return attachChild(output[i].children, child);
+        }
+    }
+    return false;
+}
+
+function buildHeirarchy(g)
+{
+    var output = [];
+    for (var i in g) {
+        g[i].children = [];
+    }
+
+    // add all the top level parents
+    for (var i in g) {
+        var node = g[i];
+        if( node.parent == null )
+        {
+            output.push(node);
+            delete(g[i]); //delete node from input because we've successfully attached it
+        }
+    }
+
+
+    var fullyOrdered;
+
+    // running attachChild for every node is not guaranteed to work the first time
+    do
+    {
+        // start out assuming things are ok
+        fullyOrdered = true;
+
+        for (var i in g) {
+            var node = g[i];
+
+            // try to attach the child.  If we are attaching a child to a parent that hasn't been attached yet, we loop through again
+            var success = attachChild(output, node);
+
+            // if a single pass fails, set this flag which will make the while go again
+            if( !success )
+            {
+                fullyOrdered = false;
+            }
+            else
+            {
+                delete(g[i]); // mark the node as attached by deleting it so we don't re-attach forever
+            }
+        }
+
+    }
+    while (!fullyOrdered);
+
+
+    return output;
+}
+
+
+
+Template.groups.groupsDebug = function()
+{
+    g = Groups.find().fetch();
+
+//    g.reverse();
+
+
+
+    return JSON.stringify(buildHeirarchy(g));
+
+
+    return [JSON.stringify(g)];
+
+
+}
