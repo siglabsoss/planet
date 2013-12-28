86918f9cd5e95c4734047426f5111731595ee96d
Benjamin Morse <bjmorse@gmail.com>
2013-12-27 22:20:35 -0800

    * adding flashAlertMessage() which is pretty slick

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -264,3 +264,33 @@ Template.dashboard.showFenceSetting = function()
 
     return o.view.showFences;
 }
+
+// Returns raw html for an alert
+// Clears a css class which causes alert to fade away after time
+Template.flashAlert.rawHtml = function()
+{
+    var m = Session.get("flashAlertMessageObject");
+    if( !m )
+        return "";
+
+    // See: http://stackoverflow.com/questions/7676356/can-twitter-bootstrap-alerts-fade-in-as-well-as-out
+    setTimeout(function(){
+        $('#myFlashAlert').removeClass('fadein');
+        setTimeout(function(){Session.set("flashAlertMessageObject", null);},1000); // remove session var after fade has completed or template rerender will instantly remove
+    }, m.options.hideAfter);
+
+    return "<div id='myFlashAlert' class='alert alert-" + m.options.type + " fade fadein floatingAlert'>"+ m.message+"</div>";
+}
+
+// Call to flash a message to the user which will fade away. The options parameter is optional
+flashAlertMessage = function(message, options)
+{
+    var def = {hideAfter:3000,type:'success'}
+
+    options = typeof options !== 'undefined' ? options : def;
+    options.hideAfter = options.hideAfter || def.hideAfter;
+    options.type = options.type || def.type;
+
+    Session.set("flashAlertMessageObject", {message:message,options:options});
+
+}
