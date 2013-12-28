a75f85e8e6a11095415e246b6fb7e7d1ee02eaa3
Benjamin Morse <bjmorse@gmail.com>
2013-12-27 19:52:30 -0800

    * draw works

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -64,6 +64,98 @@ Template.mainMap.rendered = function() {
         console.log("double clicked at " + e.latlng.lat + ", " + e.latlng.lng);
     });
 
+
+
+
+
+
+
+
+
+
+
+
+    var drawnItems = new L.FeatureGroup();
+    window.map.addLayer(drawnItems);
+
+    // Set the title to show on the polygon button
+    L.drawLocal.draw.toolbar.buttons.polygon = 'Draw a GEO fence!';
+
+    var drawControl = new L.Control.Draw({
+        position: 'topleft',
+        draw: {
+            polyline: {
+                metric: false
+            },
+            polygon: {
+                allowIntersection: false,
+                showArea: true,
+                drawError: {
+                    color: '#b00b00',
+                    timeout: 1000
+                },
+                shapeOptions: {
+                    color: '#bada55'
+                }
+            },
+            circle: {
+                shapeOptions: {
+                    color: '#662d91'
+                }
+            },
+            marker: false
+        },
+        edit: {
+            featureGroup: drawnItems,
+            remove: false
+        }
+    });
+    window.map.addControl(drawControl);
+
+    window.map.on('draw:created', function (e) {
+        var type = e.layerType,
+            layer = e.layer;
+
+        if (type === 'marker') {
+            layer.bindPopup('A popup!');
+        }
+
+        drawnItems.addLayer(layer);
+    });
+
+    window.map.on('draw:edited', function (e) {
+        var layers = e.layers;
+        var countOfEditedLayers = 0;
+        layers.eachLayer(function(layer) {
+            countOfEditedLayers++;
+        });
+        console.log("Edited " + countOfEditedLayers + " layers");
+    });
+
+//    L.DomUtil.get('changeColor').onclick = function () {
+//        drawControl.setDrawingOptions({ rectangle: { shapeOptions: { color: '#004a80' } } });
+//    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    var marker = L.marker([32, -122]).addTo(window.map);
 
 
