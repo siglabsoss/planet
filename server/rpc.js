12f9e74f06150c9bf55af045360feb5f35e42e00
Benjamin Morse <bjmorse@gmail.com>
2014-02-11 02:31:36 -0800

    * shake devices obeys bounds, runs forever once started

--- server/rpc.js
+++ server/rpc.js
@@ -1,49 +1,93 @@
 
+var devicesShaking = false;
+
+var testDeviceCount = 100;
 
 Meteor.startup(function () {
     Meteor.methods({
 
         generateTestDevices: function(){
-
-            var count = 5;
-
-            for (var i=1; i<count; i++){
+            for (var i=1; i<testDeviceCount; i++){
                 Devices.insert({lat:37.493316 + 0.1 * (-0.5 + Math.random()),
                                 lng:-122.250366 + 0.1 * (-0.5 + Math.random()),
                                 serial:i});
             }
-
         },
 
 
 
-        shakeTestDevices: function(){
+        shakeTestDevices: function(run){
 
-            var updateTimeInterval = 1000;
-            var stopShakeAfter = 20000;
+            if( run && devicesShaking ) {
+                return;
+            }
 
-            var shakeFunction = function() {
+            if( run ) {
+                devicesShaking = true;
+            } else {
+                devicesShaking = false;
+                return;
+            }
+
+            // simulation params
+
+            var spawnPoint = {lat:37.774035,lng:-122.419281};
+            var updateTimeInterval = 500;
+            // rand must be below this to move a pin, 1 is full move
+            var randomChance = 0.01;
+            // random movement maximum in lat/lng coords
+            var wanderDistance = 0.0033;
 
 
-                var count = 5;
+            var testFence = JSON.parse('[{"lat":37.70609673460725,"lng":-122.5191879272461},{"lat":37.8065289741725,"lng":-122.5191879272461},{"lat":37.8065289741725,"lng":-122.38494873046875},{"lat":37.70609673460725,"lng":-122.38494873046875}]');
+            var fenceObject = closedJSTSGeomFromPoints(testFence);
 
-                for (var i=1; i<count; i++){
-                    Devices.update(Devices.find({serial: i}).fetch()[0]._id, {$inc: {lat: 0.01 * (-0.5 + Math.random())}});
-                    Devices.update(Devices.find({serial: i}).fetch()[0]._id, {$inc: {lng: 0.01 * (-0.5 + Math.random())}});
+
+            var shakeFunction;
+            shakeFunction = function() {
+
+
+                for (var i=1; i<testDeviceCount; i++){
+
+                    if( Math.random() >= randomChance )
+                        continue;
+
+                    var device = Devices.findOne({serial: i});
+                    var pointObject = coordJSTS(device);
+
+                    // ok to pass null for 3rd param unless it's a circle (oh god)
+                    var locationOk = deviceInsideFence("rectangle", device, null, fenceObject);
+
+                    var query = null;
+
+                    if( !locationOk ) {
+                        // query to reset device
+                        // spawnPoint is a ready-to-go object
+                        query = {$set:
+                            spawnPoint
+                        };
+                    } else {
+                        // query to jiggle it
+                        // this is one way to update devices, however I don't know if they are leaving the fence cuz these numbers are relative
+                        query = {$inc: {
+                            lat: wanderDistance * (-0.5 + Math.random()),
+                            lng: wanderDistance * (-0.5 + Math.random())
+                        }};
+                    }
+
+                    // run either query
+                    Devices.update(device._id, query);
                 }
 
+                if( devicesShaking )
+                {
+                    Meteor.setTimeout(shakeFunction, updateTimeInterval);
+                }
 
             };
 
-            var interval = Meteor.setInterval(shakeFunction, updateTimeInterval);
-
-            var stopFunction = function() {
-                   Meteor.clearInterval(interval);
-            };
-
-            Meteor.setTimeout(stopFunction, stopShakeAfter);
+            shakeFunction();
         },
-
 
 
 
