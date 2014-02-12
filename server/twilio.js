67105beb766245dae06eb72f04e3c6e9dbba551b
Benjamin Morse <bjmorse@gmail.com>
2014-02-11 17:16:34 -0800

    * adding gitignore for settings file * adding sendText

--- server/twilio.js
+++ server/twilio.js
@@ -1,5 +1,26 @@
-sendText = function(to, message) {
+// file scope
+var client = null;
+
+function setupClient() {
+    if(client) {
+        return;
+    }
+
+    client = Npm.require('twilio')(Meteor.settings.TWILIO_SID, Meteor.settings.TWILIO_TOKEN);
+}
+
+sendText = function(to, messageText) {
+    setupClient();
+
+    client.messages.create({
+        to: to,
+        from: "+16193659929", // also +18584141421",
+        body: messageText
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+
     console.log(to);
-    console.log(message);
-    console.log(Meteor.settings.TWILIO_KEY);
+//    console.log(message);
+    console.log(Meteor.settings.TWILIO_SID);
 }
